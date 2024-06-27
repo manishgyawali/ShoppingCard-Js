050cdb4e5b6172531dc02f1383269ff5a186bfc1
manishgyawali <manishgyawali345@gmail.com>
2024-06-27 19:40:51 +0545

    new commit

--- script.js
+++ script.js
@@ -49,7 +49,18 @@ let shoppingcard = [
     Discount: "Discount 20% OFF",
     image: "./mac.png",
   },
+  {
+    Name: "Ac",
+    Details:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita laborum, vitae neque  ",
+    Price: "$1500",
+    Rating: [1, 2, 3, 4, 5],
+    button: "ADD TO CART",
+    Discount: "Discount 20% OFF",
+    image: "./ac.png",
+  },
 ];
+
 let display = document.getElementById("nameid");
 
 shoppingcard.map((value, index) => {
@@ -59,6 +70,9 @@ shoppingcard.map((value, index) => {
       <svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24"><path fill="currentColor" d="m8.85 16.825l3.15-1.9l3.15 1.925l-.825-3.6l2.775-2.4l-3.65-.325l-1.45-3.4l-1.45 3.375l-3.65.325l2.775 2.425zm-1.525 2.098l1.24-5.313l-4.123-3.572l5.431-.47L12 4.557l2.127 5.01l5.43.47l-4.123 3.572l1.241 5.313L12 16.102zM12 12.25"/></svg>
       `;
   });
+
+
+  
 
   display.innerHTML += `
 
